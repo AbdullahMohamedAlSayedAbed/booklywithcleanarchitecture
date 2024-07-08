f1ad956e27263de9b32e427efc08cb8a463debce
Abdullah Abed <abedabdallah834@gmail.com>
2024-07-08 10:31:39 +0300

    add book entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,14 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String author;
+  final num price;
+  final num rating;
+
+  const BookEntity(
+      {required this.image,
+      required this.title,
+      required this.author,
+      required this.price,
+      required this.rating});
+}
